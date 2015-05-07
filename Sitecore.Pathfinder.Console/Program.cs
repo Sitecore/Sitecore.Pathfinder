93e25b6c65c8160d6efdb52649077ef4bb7c4e26
Jakob Christensen <jc@sitecore.net>
2015-05-07 12:21:32 +0200

    Small change

--- Sitecore.Pathfinder.Console/Program.cs
+++ Sitecore.Pathfinder.Console/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace Sitecore.Pathfinder
 {
   using System.Diagnostics;
+  using Sitecore.Pathfinder.Diagnostics;
 
   internal class Program
   {
-    private static void Main(string[] args)
+    private static void Main([NotNull] string[] args)
     {
       Trace.Listeners.Add(new ConsoleTraceListener());
 
