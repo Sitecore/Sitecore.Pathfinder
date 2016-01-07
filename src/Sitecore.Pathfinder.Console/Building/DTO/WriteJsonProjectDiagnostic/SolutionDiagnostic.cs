81382824aedfb0fae549adc83a993ffed56647b1
Alin Marius Pârjolea <amp@sitecore.net>
2016-01-07 09:52:39 +0100

    fixes after recommendations

---
+++ src/Sitecore.Pathfinder.Console/Building/DTO/WriteJsonProjectDiagnostic/SolutionDiagnostic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Sitecore.Pathfinder.Diagnostics;
+
+namespace Sitecore.Pathfinder.Building.DTO.WriteJsonProjectDiagnostic
+{
+    public class SolutionDiagnostic
+    {
+        [NotNull]
+        public string SolutionName { get; set; }
+
+        [NotNull]
+        public string SolutionLocation { get; set; }
+
+        [NotNull]
+        [ItemNotNull]
+        public IList<ProjectDiagnostic> ProjectDiagnostics { get; set; }
+    }
+}
--- src/Sitecore.Pathfinder.Console/Building/Tasks/WriteJsonProjectDiagnostics.cs
+++ src/Sitecore.Pathfinder.Console/Building/Tasks/WriteJsonProjectDiagnostics.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using Sitecore.Pathfinder.Building.DTO.WriteJsonProjectDiagnostic;
 using Sitecore.Pathfinder.Diagnostics;
 using Sitecore.Pathfinder.Extensions;
 
@@ -25,11 +26,11 @@ namespace Sitecore.Pathfinder.Building.Tasks
 
         public override void WriteHelp(HelpWriter helpWriter)
         {
-            helpWriter.Summary.Write("Checks the project for warnings and errors.");
-            helpWriter.Remarks.Write("SETTINGS:" + Environment.NewLine);
-            helpWriter.Remarks.Write("  write-json-project-diagnostics:output-path - /op = Insert path for outputting the json file). " + Environment.NewLine);
-            helpWriter.Remarks.Write("  write-json-project-diagnostics:disabled-categories - Disables checker categories (Items, Fields, Templates, TemplateFields, Media)." + Environment.NewLine);
-            helpWriter.Remarks.Write("  write-json-project-diagnostics:disabled-checkers - Disables specific checkers." + Environment.NewLine);
+            helpWriter.Summary.WriteLine("Checks the project for warnings and errors.");
+            helpWriter.Remarks.WriteLine("SETTINGS:");
+            helpWriter.Remarks.WriteLine("  write-json-project-diagnostics:output-path - /op = Insert path for outputting the json file). ");
+            helpWriter.Remarks.WriteLine("  write-json-project-diagnostics:disabled-categories - Disables checker categories (Items, Fields, Templates, TemplateFields, Media).");
+            helpWriter.Remarks.WriteLine("  write-json-project-diagnostics:disabled-checkers - Disables specific checkers.");
         }
 
         protected virtual void WriteJsonDiagnostics([NotNull] IBuildContext context)
@@ -77,45 +78,8 @@ namespace Sitecore.Pathfinder.Building.Tasks
         [NotNull]
         protected virtual string GetOutputPath([NotNull]IBuildContext context)
         {
-            string path = context.Configuration.GetString("op");
+            string path = context.Configuration.GetString("output-path");
             return path ?? string.Empty;
         }
     }
-
-    internal class SolutionDiagnostic
-    {
-        [NotNull]
-        public string SolutionName { get; set; }
-        [NotNull]
-        public string SolutionLocation { get; set; }
-        [NotNull]
-        [ItemNotNull]
-        public IList<ProjectDiagnostic> ProjectDiagnostics { get; set; }
-    }
-
-    internal class ProjectDiagnostic
-    {
-        [NotNull]
-        public string ProjectId { get; set; }
-        [NotNull]
-        public string ProjectName { get; set; }
-        [NotNull]
-        public string ProjectDirectory { get; set; }
-        [NotNull]
-        [ItemNotNull]
-        public IList<Diagnostic> Diagnostics { get; set; }
-    }
-
-    internal class Diagnostic
-    {
-        public Severity Severity { get; set; }
-        public int Code { get; set; }
-        [NotNull]
-        public string FileName { get; set; }
-        [NotNull]
-        public string TextSpan { get; set; }
-        [NotNull]
-        public string Text { get; set; }
-    }
-
 }
